e87cb93b47351f0798f1b0c576bec0803dc87656
benbbao123 <95001426+benbbao123@users.noreply.github.com>
2021-12-03 12:46:00 -0500

    model loading

--- DecisionTree.py
+++ DecisionTree.py
@@ -2,30 +2,29 @@ import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 import timeit
 
-start = timeit.default_timer()
+def TrainModel():
 
-df_train = pd.read_csv('Data/StreetSigns/csvfiles/Train.csv')
-df_test1 = pd.read_csv('Data/StreetSigns/csvfiles/Test.csv')
-df_meta = pd.read_csv('Data/StreetSigns/csvfiles/Meta.csv')
+  start = timeit.default_timer()
 
-df_train.head()
-df_test1.head()
-df_meta.head()
+  df_train = pd.read_csv('Data/StreetSigns/csvfiles/Train.csv')
+  df_meta = pd.read_csv('Data/StreetSigns/csvfiles/Meta.csv')
 
-df = df_train.merge(df_meta, on="ClassId")
-df_test = df_test1.merge(df_meta, on="ClassId")
+  df_train.head()
+  df_meta.head()
 
-feature_cols = ['Roi.X1', 'Roi.Y1', 'Roi.X2', 'Roi.Y2', 'ShapeId', 'ColorId']
+  df = df_train.merge(df_meta, on="ClassId")
 
-X_train = df[feature_cols]
-y_train = df.ClassId
+  feature_cols = ['Roi.X1', 'Roi.Y1', 'Roi.X2', 'Roi.Y2', 'ShapeId', 'ColorId']
 
-X_test = df_test[feature_cols]
-y_test = df_test.ClassId
+  X_train = df[feature_cols]
+  y_train = df.ClassId
 
-clf = DecisionTreeClassifier(max_depth=9, min_samples_leaf=5)
-clf = clf.fit(X_train, y_train)
+  clf = DecisionTreeClassifier(max_depth=9, min_samples_leaf=5)
+  clf = clf.fit(X_train, y_train)
 
-stop = timeit.default_timer()
+  filename = 'DecisionTree_model.sav'
+  joblib.dump(clf, filename)
 
-print(f'Decision Tree Model trained in {round(stop - start, 2)} seconds')
+  stop = timeit.default_timer()
+
+  print(f'Decision Tree Model trained in {round(stop - start, 2)} seconds')
