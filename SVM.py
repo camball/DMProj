f552a9a7194492990ced65aaa227166c7f053640
camball <67163069+camball@users.noreply.github.com>
2021-12-02 20:41:22 -0500

    Clean-up/increase readability; explain attempts

--- SVM.py
+++ SVM.py
@@ -4,7 +4,6 @@ CIS 4930 Project File: Support Vector Machine Classification
 This script closely follows the write-up by Vegi Shanmukh at 
 https://medium.com/analytics-vidhya/image-classification-using-machine-learning-support-vector-machine-svm-dc7a0ec92e01
 yet was configured to match the data from https://www.kaggle.com/meowmeowmeowmeowmeow/gtsrb-german-traffic-sign?select=Train
-
 """
 
 import time
@@ -57,76 +56,165 @@ df['Target'] = target
 x_train = df.iloc[:,:-1] # input data (feature vectors) (all except last column)
 y_train = df.iloc[:,-1]  # output data (class labels with key 'Target') (only last column)
 
-#
-# Phase 1 code (input data configuration) takes around 27 seconds to run.
 print(f'[Phase 1 completed in {round(time.time() - startTime, 2)} seconds]')
-#
 
 
 
 startTime = time.time()
 
 # ------------------------------------------------------------------------------------------------
-# Phase 2: Building the model
+# Phase 2: Building/Training the model
 # ------------------------------------------------------------------------------------------------
 
-from sklearn.svm import SVC, LinearSVC
+'''
+## ATTEMPT 1
+## Here was my initial attempt. I was using GridSearchCV to try and find the best hyperparameters.
 
-# from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
 
-# construct our classifier
-# svc = SVC(kernel='rbf', probability=True, cache_size=1900, verbose=True, max_iter=5)
+param_grid = {
+    'C': [0.1, 1, 10, 100],
+    'gamma': [0.0001, 0.001, 0.1, 1],
+    'kernel': ['rbf', 'poly']
+}
 
-svc = LinearSVC(tol=0.01, verbose=5, max_iter=5)
-
-# param_grid = {  
-#     'C': [0.1, 1, 10, 100],
-#     'gamma': [0.0001, 0.001, 0.1, 1],
-#     'kernel': ['rbf', 'poly']
-# }
-
-svc.fit(x_train, y_train)
-
-# param_grid = {  
-#     'C': [1],
-#     'gamma': [1],
-#     'kernel': ['rbf']
-# }
+svc = SVC(probability=True)
 
 # construct our model using the classifier and different parameter values. When we call fit(),
 # the model will try the parameters to find the best ones. I specify the n_jobs parameter to 
 # attempt to speed up fit() process via parallelization. My computer crashed when I
 # parallelized across all cpu cores, so I'm doing two less than the max, for a total of ten.
-# model = GridSearchCV(svc, param_grid, n_jobs=os.cpu_count()-2, verbose=5)
-# model = GridSearchCV(svc, param_grid, cv=2, verbose=5)
+model = GridSearchCV(svc, param_grid)
 
-#
-# Phase 2 code (model construction) takes around 6.5 seconds to run.
+model.fit(x_train, y_train)
+'''
+
+# ------------------------------------------------------------------------------------------------
+
+'''
+## ATTEMPT 2
+## Attempt 1 was taking a scarily long amount of time. I dug in and realized that with the 
+## parameter grid configured the way it was in atttempt 1, it was running the model.fit() method
+## 160 times - there are 32 different hyperparameter combinations and with GridSearchCV's
+## default of 5-fold cross validation, it was running 32 * 5 = 160 times. So in this attempt, I
+## changed the param_grid to only have one combination of parameters so that it would only run 
+## 1 * 5 = 5 times.
+
+from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
+
+param_grid = {  
+    'C': [1],
+    'gamma': [1],
+    'kernel': ['rbf']
+}
+
+svc = SVC(probability=True)
+model = GridSearchCV(svc, param_grid)
+
+model.fit(x_train, y_train)
+'''
+
+# ------------------------------------------------------------------------------------------------
+
+'''
+## ATTEMPT 3
+## I realized that I could probably speed up the training if I took advantage of parallelization,
+## so I specified the n_jobs parameter to take advantage of all my cpu cores. My computer had a
+## hard crash with error "kernel panic" when doing that, so I set n_jobs equal to two less than
+## the number of cpu cores in my computer. My computer crashed again, so I went into my computer
+## and cleared out some storage in case it was out of RAM and was also out of swap memory. I 
+## monitored the activity monitor to see how much RAM/CPU was being used, and it was essentially
+## maxed out.
+
+from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
+
+param_grid = {  
+    'C': [1],
+    'gamma': [1],
+    'kernel': ['rbf']
+}
+
+svc = SVC(probability=True)
+model = GridSearchCV(svc, param_grid, n_jobs=os.cpu_count()-2, verbose=5)
+
+model.fit(x_train, y_train)
+'''
+
+# ------------------------------------------------------------------------------------------------
+
+'''
+## ATTEMPT 4
+## After failing to take advantage of parallelization, I thought I should try reducing the calls
+## to model.fit() once again. To do this, I set the cv parameter to only perform two-fold
+## cross-validation resulting in two calls to fit().
+
+from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
+
+param_grid = {  
+    'C': [1],
+    'gamma': [1],
+    'kernel': ['rbf']
+}
+
+svc = SVC(probability=True)
+model = GridSearchCV(svc, param_grid, cv=2, verbose=5)
+
+model.fit(x_train, y_train)
+'''
+
+# ------------------------------------------------------------------------------------------------
+
+'''
+## ATTEMPT 5
+## Attempt 4 still took absolutely forever and never terminated, so I dropped the use of 
+## GridSearchCV altogether, resulting in this attempt's code.
+
+from sklearn.svm import SVC
+
+svc = SVC(kernel='rbf', probability=True)
+
+svc.fit(x_train, y_train)
+'''
+
+# ------------------------------------------------------------------------------------------------
+
+'''
+## ATTEMPT 6
+## The pattern continues... attempt 5 still never terminated, so I increased the cache size to
+## see if I could get any speedup from that, and I set the max iterations to a ridiculously low
+## number just as a proof-of-concept to see if I could get the process to terminate, which it
+## never did.
+
+from sklearn.svm import SVC
+
+svc = SVC(kernel='rbf', probability=True, cache_size=1900, verbose=True, max_iter=5)
+
+svc.fit(x_train, y_train)
+'''
+
+# ------------------------------------------------------------------------------------------------
+
+## ATTEMPT 7
+## Here is my last attempt, which was to use LinearSVC instead of SVC. Per sklearn.svm.SVC's
+## docstring, "The fit time scales at least quadratically with the number of samples and may be
+## impractical beyond tens of thousands of samples. For large datasets consider using ~sklearn.svm.LinearSVC"
+## As such, I am trying LinearSVC here on our decently large dataset. I set the tolerance to a
+## rather large number and kept the max_iter set to a very low number, again as a 
+## proof-of-concept to see if it would terminate/converge. The process did terminate, with a 
+## message of "ConvergenceWarning: Liblinear failed to converge, increase the number of
+## iterations." Another group member, Steven, tried the code with max_iter=100 and still got 
+## the same ConvergenceWarning. This may be because the data is simply not linearly-seperable.
+
+from sklearn.svm import LinearSVC
+
+# construct our classifier
+svc = LinearSVC(tol=0.01, verbose=5, max_iter=5)
+
+svc.fit(x_train, y_train)
+
+
 print(f'[Phase 2 completed in {round(time.time() - startTime, 2)} seconds]')
-#
-
-
-
-# startTime = time.time()
-
-# # model.fit(x_train, y_train)
-
-# print(f'[Phase 3 completed in {round(time.time() - startTime, 2)} seconds]')
-
-
-# ------------------------------------------------------------------------------------------------
-# Phase 3: Train the model
-# ------------------------------------------------------------------------------------------------
-
-# from sklearn.model_selection import train_test_split
-
-# x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=77, stratify=y)
-
-# print(type(x_train), type(x_test), type(y_train), type(y_test))
-
-# print('Splitted Successfully')
-
-# model.fit(x_train, y_train)
-
-# print('The Model is trained well with the given images')
-# model.best_params_ contains the best parameters obtained from GridSearchCV
