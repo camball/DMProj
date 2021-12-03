7d8bf3770a1408be2b9647b27dbc92c00617bf8a
camball <67163069+camball@users.noreply.github.com>
2021-12-03 18:18:04 -0500

    Cleaning

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,20 +1,19 @@
-import joblib
-import pandas as pd
-from matplotlib import pyplot as plt
-from sklearn.tree import DecisionTreeClassifier
 import timeit
-
-#timer start
 start = timeit.default_timer()
 
-#merge meta and train dataset to increase accuracy
+
+import joblib
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+
+# merge meta and train dataset to increase accuracy
 df_train = pd.read_csv('StreetSignModel/Data/StreetSigns/csvfiles/Train.csv')
 df_meta = pd.read_csv('StreetSignModel/Data/StreetSigns/csvfiles/Meta.csv')
 
 df_train.head()
 df_meta.head()
 
-#merge meta and train dataset to increase accuracy
+# merge meta and train dataset to increase accuracy
 df = df_train.merge(df_meta, on="ClassId")
 
 feature_cols = ['Roi.X1', 'Roi.Y1', 'Roi.X2', 'Roi.Y2', 'ShapeId', 'ColorId']
@@ -22,15 +21,13 @@ feature_cols = ['Roi.X1', 'Roi.Y1', 'Roi.X2', 'Roi.Y2', 'ShapeId', 'ColorId']
 X_train = df[feature_cols]
 y_train = df.ClassId
 
-#create decision tree training model
+# create decision tree training model
 clf = DecisionTreeClassifier(max_depth=9, min_samples_leaf=5)
 clf = clf.fit(X_train, y_train)
 
-#store model
+# store model
 filename = 'Models/DecisionTree_model.sav'
 joblib.dump(clf, filename)
 
-#timer end
-stop = timeit.default_timer()
 
-print(f'Decision Tree Model trained in {round(stop - start, 2)} seconds')
+print(f'Decision Tree Model trained in {round(timeit.default_timer() - start, 2)} seconds')
