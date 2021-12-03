6192167f4628eb356928c0fe17189cf2080f8ef2
benbbao123 <95001426+benbbao123@users.noreply.github.com>
2021-12-03 11:49:06 -0500

    added timer

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,8 +1,8 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
-from sklearn import tree
-from matplotlib import pyplot as plt
+import timeit
+
+start = timeit.default_timer()
 
 df_train = pd.read_csv('Data/StreetSigns/csvfiles/Train.csv')
 df_test1 = pd.read_csv('Data/StreetSigns/csvfiles/Test.csv')
@@ -16,7 +16,6 @@ df = df_train.merge(df_meta, on="ClassId")
 df_test = df_test1.merge(df_meta, on="ClassId")
 
 feature_cols = ['Roi.X1', 'Roi.Y1', 'Roi.X2', 'Roi.Y2', 'ShapeId', 'ColorId']
-features = ['Width', 'Height', 'Roi.X1', 'Roi.Y1', 'Roi.X2', 'Roi.Y2']
 
 X_train = df[feature_cols]
 y_train = df.ClassId
@@ -27,7 +26,6 @@ y_test = df_test.ClassId
 clf = DecisionTreeClassifier(max_depth=9, min_samples_leaf=5)
 clf = clf.fit(X_train, y_train)
 
-y_pred = clf.predict(X_test)
+stop = timeit.default_timer()
 
-error_rate = 1 - accuracy_score(y_test, y_pred)
-print('Error rate', error_rate)
+print(f'Decision Tree Model trained in {round(stop - start, 2)} seconds')
