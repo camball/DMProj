5e710c71c0352970ab73349d7b9073c8e1adef51
camball <67163069+camball@users.noreply.github.com>
2021-12-03 17:44:40 -0500

    Cleaning

--- Predictions.py
+++ Predictions.py
@@ -1,64 +1,64 @@
-# import cv2
 import time
 import timeit
 
 import joblib
 import numpy as np
 import pandas as pd
+from tensorflow import math
 from tensorflow.keras.models import load_model
-import tensorflow as tf
-from tensorflow.keras.applications.vgg16 import preprocess_input
+from tensorflow.keras.applications import vgg16, mobilenet
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
 from tabulate import tabulate
 from sklearn.metrics import classification_report, confusion_matrix
 
 
-Categories = {0: '(20km/h)',
-              1: '(30km/h)',
-              2: '(50km/h)',
-              3: '(60km/h)',
-              4: '(70km/h)',
-              5: '(80km/h)',
-              6: 'End of (80km/h)',
-              7: '(100km/h)',
-              8: '(120km/h)',
-              9: 'No passing',
-              10: 'No overtaking by trucks',
-              11: 'Crossroads ahead side roads to right and left',
-              12: 'Priority road',
-              13: 'Yield',
-              14: 'Stop',
-              15: 'No vehicles',
-              16: 'No entry for trucks',
-              17: 'No entry',
-              18: 'other danger',
-              19: 'curve left',
-              20: 'curve right',
-              21: 'Double curve, first left',
-              22: 'uneven road',
-              23: 'Slippery road',
-              24: 'Road narrows on the right',
-              25: 'Road work',
-              26: 'Traffic signals',
-              27: 'Pedestrians',
-              28: 'Children crossing',
-              29: 'Bicycles crossing',
-              30: 'Beware of ice/snow',
-              31: 'Wild animals crossing',
-              32: 'End all prohibitions',
-              33: 'Turn right ahead',
-              34: 'Turn left ahead',
-              35: 'Ahead only',
-              36: 'Go straight or right',
-              37: 'Go straight or left',
-              38: 'Keep right',
-              39: 'Keep left',
-              40: 'Roundabout',
-              41: 'End of overtaking prohibition',
-              42: 'End no overtaking for large vehicles'}
+Categories = {
+    0: '(20km/h)',
+    1: '(30km/h)',
+    2: '(50km/h)',
+    3: '(60km/h)',
+    4: '(70km/h)',
+    5: '(80km/h)',
+    6: 'End of (80km/h)',
+    7: '(100km/h)',
+    8: '(120km/h)',
+    9: 'No passing',
+    10: 'No overtaking by trucks',
+    11: 'Crossroads ahead side roads to right and left',
+    12: 'Priority road',
+    13: 'Yield',
+    14: 'Stop',
+    15: 'No vehicles',
+    16: 'No entry for trucks',
+    17: 'No entry',
+    18: 'other danger',
+    19: 'curve left',
+    20: 'curve right',
+    21: 'Double curve, first left',
+    22: 'uneven road',
+    23: 'Slippery road',
+    24: 'Road narrows on the right',
+    25: 'Road work',
+    26: 'Traffic signals',
+    27: 'Pedestrians',
+    28: 'Children crossing',
+    29: 'Bicycles crossing',
+    30: 'Beware of ice/snow',
+    31: 'Wild animals crossing',
+    32: 'End all prohibitions',
+    33: 'Turn right ahead',
+    34: 'Turn left ahead',
+    35: 'Ahead only',
+    36: 'Go straight or right',
+    37: 'Go straight or left',
+    38: 'Keep right',
+    39: 'Keep left',
+    40: 'Roundabout',
+    41: 'End of overtaking prohibition',
+    42: 'End no overtaking for large vehicles'
+}
 
 # unsplit_path = "UnsplitData"
 # folders = os.listdir(unsplit_path)
@@ -83,18 +83,22 @@ test_path = "StreetSignModel/Data/StreetSigns/Test"
 StreetSignModelVgg16 = load_model("Models/MLModelVGG16.h5")
 StreetSignMobileNet = load_model("Models/StreetSignMobileNet.h5")
 
-VGG16test_batches = ImageDataGenerator(preprocessing_function=preprocess_input).flow_from_directory(directory=test_path,
-                                                                                                    target_size=(
-                                                                                                        224, 224),
-                                                                                                    classes=[f'{n}' for
-                                                                                                             n in
-                                                                                                             range(43)],
-                                                                                                    batch_size=32,
-                                                                                                    shuffle=False)
+VGG16test_batches = ImageDataGenerator(preprocessing_function=vgg16.preprocess_input)
+VGG16test_batches.flow_from_directory(
+    directory=test_path, 
+    target_size=(224, 224),
+    classes=[f'{n}' for n in range(43)],
+    batch_size=32,
+    shuffle=False
+)
 
-MobileNetTestBatches = ImageDataGenerator(
-    preprocessing_function=tf.keras.applications.mobilenet.preprocess_input).flow_from_directory(
-    directory=test_path, target_size=(224, 224), batch_size=32, shuffle=False)
+MobileNetTestBatches = ImageDataGenerator(preprocessing_function=mobilenet.preprocess_input)
+MobileNetTestBatches.flow_from_directory(
+    directory=test_path, 
+    target_size=(224, 224), 
+    batch_size=32, 
+    shuffle=False
+)
 
 print("Starting evaluation for ML Model VGG16")
 starttime = time.time()
@@ -112,7 +116,7 @@ print(" ")
 print(" ")
 print(" ")
 
-con_mat = tf.math.confusion_matrix(labels=VGG16test_batches.labels, predictions=y_pred).numpy()
+con_mat = math.confusion_matrix(labels=VGG16test_batches.labels, predictions=y_pred).numpy()
 con_mat_norm = np.around(con_mat.astype('float') / con_mat.sum(axis=1)[:, np.newaxis], decimals=2)
 con_mat_df = pd.DataFrame(con_mat_norm, index=Categories, columns=Categories)
 figure = plt.figure(figsize=(16, 12))
@@ -137,7 +141,7 @@ y_pred = np.argmax(StreetSignMobileNet.predict(MobileNetTestBatches), axis=-1)
 MobileNetPredictionTime = time.time() - starttime
 print("Finished predictions for MobileNet Model. Total Time: ", MobileNetPredictionTime, "Seconds")
 
-con_mat = tf.math.confusion_matrix(labels=MobileNetTestBatches.labels, predictions=y_pred).numpy()
+con_mat = math.confusion_matrix(labels=MobileNetTestBatches.labels, predictions=y_pred).numpy()
 con_mat_norm = np.around(con_mat.astype('float') / con_mat.sum(axis=1)[:, np.newaxis], decimals=2)
 con_mat_df = pd.DataFrame(con_mat_norm, index=Categories, columns=Categories)
 figure = plt.figure(figsize=(16, 12))
@@ -161,14 +165,14 @@ feature_cols = ['Roi.X1', 'Roi.Y1', 'Roi.X2', 'Roi.Y2', 'ShapeId', 'ColorId']
 
 df_test1.head()
 
-#merge test and meta to match training data
+# merge test and meta to match training data
 df_test = df_test1.merge(df_meta, on="ClassId")
 
 
 X_test = df_test[feature_cols]
 y_test = df_test.ClassId
 
-#load decision tree model
+# load decision tree model
 clf = joblib.load('Models/DecisionTree_model.sav')
 
 print("Starting Evaluation for Decision Tree Model")
@@ -184,8 +188,8 @@ y_pred = clf.predict(X_test)
 DecisionTreePredictTime = timeit.default_timer() - starttime
 print("Finished predictions for Decision Tree Model. Total Time: ", DecisionTreePredictTime, "Seconds")
 
-#decision tree model prediciton confusion matrix heat map
-con_mat = tf.math.confusion_matrix(labels=y_test, predictions=y_pred).numpy()
+# decision tree model prediciton confusion matrix heat map
+con_mat = math.confusion_matrix(labels=y_test, predictions=y_pred).numpy()
 con_mat_norm = np.around(con_mat.astype('float') / con_mat.sum(axis=1)[:, np.newaxis], decimals=2)
 con_mat_df = pd.DataFrame(con_mat_norm, index=Categories, columns=Categories)
 figure = plt.figure(figsize=(16, 12))
@@ -197,14 +201,20 @@ plt.show()
 
 
 
-CummulativeResults = [
+CumulativeResults = [
     ["VGG 16", VGG16Score[0], VGG16Score[1], VGG16EvaluationTime, VGG16PredictionTime],
     ["MobileNet", MobileNetScore[0], MobileNetScore[1], MobileNetEvaluationTime, MobileNetPredictionTime],
     ["Decision Tree", "N/A", score, DecisionTreeEvaluationTime, DecisionTreePredictTime]
 ]
 
 print("Final Results: ")
-print(tabulate(CummulativeResults, headers=["Model Name", "Final Loss", "Test Accuracy", "Total evaluation time ",
-                                            "Total Prediction time"]))
-
-
+print(
+    tabulate(CumulativeResults, headers=[
+            "Model Name", 
+            "Final Loss", 
+            "Test Accuracy", 
+            "Total evaluation time ",
+            "Total Prediction time"
+        ]
+    )
+)
